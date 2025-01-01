bbf63a169839efabfe0685a3abb01fa5e8ed61dd
Santosh Bhattarai <santoshbhattarai287@gmail.com>
2025-01-01 16:34:21 +0545

    CreateFunctionilityWithVM

--- BookStore.DataAccess/Migrations/20241231085634_addimageUrl.Designer.cs
+++ BookStore.DataAccess/Migrations/20241231085634_addimageUrl.Designer.cs
@@ -2,6 +2,7 @@
 using BookStore.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -9,9 +10,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace BookStore.DataAccess.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20241231085634_addimageUrl")]
+    partial class addimageUrl
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- BookStore/Areas/Admin/Controllers/ProductController.cs
+++ BookStore/Areas/Admin/Controllers/ProductController.cs
@@ -1,6 +1,8 @@
 ﻿using BookStore.DataAccess.Repository.IRepository;
 using BookStore.Models.Models;
+using BookStore.Models.ViewModel;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace BookStore.Areas.Admin.Controllers
 {
@@ -15,20 +17,33 @@ namespace BookStore.Areas.Admin.Controllers
         public IActionResult Index()
         {
             List<Product> products= _unitOfWork.Product.GetAll().ToList();
+          
             return View(products);
         }
 
         public IActionResult Create()
         {
-            return View();
+            // IEnumerable<SelectListItem> CategoryList = 
+            //ViewBag.CategoryList = CategoryList;
+
+            ProductVM productVM = new()
+            {
+                CategoryList = _unitOfWork.Category.GetAll().Select(u => new SelectListItem
+                {
+                    Text = u.Name,
+                    Value = u.Id.ToString()
+                }),
+                Product = new Product()
+            };
+            return View(productVM);
         }
 
         [HttpPost]
-        public IActionResult Create(Product obj)
+        public IActionResult Create(ProductVM obj)
         {
             if (ModelState.IsValid)
             {
-                _unitOfWork.Product.Add(obj);
+                _unitOfWork.Product.Add(obj.Product);
                 _unitOfWork.save();
                 TempData["success"] = "Product created successfully";
                 return RedirectToAction("Index");
