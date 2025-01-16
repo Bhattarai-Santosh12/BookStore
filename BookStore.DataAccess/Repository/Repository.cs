936e913917914d5367d6474f8986aab6499735fa
Santosh Bhattarai <santoshbhattarai287@gmail.com>
2025-01-16 09:11:46 +0545

    fixing-count-Update-Bugs

--- BookStore.DataAccess/Repository/Repository.cs
+++ BookStore.DataAccess/Repository/Repository.cs
@@ -2,6 +2,7 @@
 using BookStore.DataAccess.Repository.IRepository;
 using Microsoft.EntityFrameworkCore;
 using System.Linq.Expressions;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory;
 
 namespace BookStore.Repository
 {
@@ -21,9 +22,16 @@ namespace BookStore.Repository
           dbSet.Add(entity);
         }
 
-        public T Get(Expression<Func<T, bool>> filter, string? includeProperties = null)
+        public T Get(Expression<Func<T, bool>> filter, string? includeProperties = null, bool tracked = false)
         {
-           IQueryable<T> query = dbSet;
+            IQueryable<T> query;
+            if(tracked){
+                query= dbSet.AsNoTracking();
+            }
+            else
+            {
+               query= dbSet.AsNoTracking();
+            }
             query = query.Where(filter);
             if (!string.IsNullOrEmpty(includeProperties))
             {
@@ -34,6 +42,7 @@ namespace BookStore.Repository
                 }
             }
             return query.FirstOrDefault();
+
         }
 
         public IEnumerable<T> GetAll(string? includeProperties = null)
